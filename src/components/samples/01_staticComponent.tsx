636d6bb67c61fbe4c097d71c714fd0260fbc6164
20105650 <20105650@mail.wit.ie>
2024-03-27 19:45:53 +0000

    Exercise 1 completed

---
+++ src/components/samples/01_staticComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Demo: React.FC = () => {
+    return (
+        <div>
+            <h1>Dynamic Languages</h1>
+            <ul>
+                <li>Python</li>
+                <li>Symphony</li>
+                <li>Laravel</li>
+                <li>PHP</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Demo;
